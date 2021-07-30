543486d45251f7806c075ec5f7136f364ef2f04c
DargonXUan <37977109+DargonXuan@users.noreply.github.com>
2021-07-30 16:20:20 +0800

    fix(match_image): 类型判断注释

--- lib/utils.py
+++ lib/utils.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# @author: SkuMoe
+# @author: Green Sulley
 # @license: MIT License
 # @contact: sukeycz0@gmail.com
 # @software: PyCharm
 # @project : ArknightsAutoHelper
+# @github: GreenSulley/ArknightsAutoHelper
 # @file: utils.py
-# @time: 2021/7/29 17:00
+# @createtime: 2021/7/29 17:00 Green Sulley
+# @lastupdate: 2021/7/30 11:37 Green Sulley
 # @desc: Arknights Auto Helper based on ADB and Python
 
 import cv2
@@ -15,11 +17,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def img_cv2(img_path):
+def img_cv2(img_path: str):
     return cv2.imread(img_path)
 
 
-def img_base64(img_path):
+def img_np_cv2(img_path: str):
+    """
+    中文路径
+    :param img_path:
+    :return:
+    """
+    return cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+
+def img_base64(img_path: str):
     with open(img_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
@@ -28,17 +39,28 @@ def cv2_to_base64(image):
     return base64.b64encode(cv2.imencode('.jpg', image)[1].tostring())
 
 
-def base64_to_cv2(base64_str):
+def base64_to_cv2(base64_str: str):
     return cv2.imdecode(np.fromstring(base64.b64decode(base64_str), np.uint8),
                         cv2.IMREAD_COLOR)
 
 
-def tap_match_image(temp, target):
+def exist_match_image(temp, target):
+    # False: 0.13017292320728302
+    # True: 9.214816964231431e-05
+    result = cv2.matchTemplate(target, temp, cv2.TM_SQDIFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    if min_val > 0.1:
+        return False
+    return True
+
+
+def match_image(temp, target):
     h, w, s = temp.shape
     result = cv2.matchTemplate(target, temp, cv2.TM_SQDIFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    print(min_val, min_loc)
-    print(h / 2)
+    # print(min_val, min_loc)
+    if min_val > 0.1:
+        return False
     return min_loc[0] + (w / 2), min_loc[1] + (h / 2)
 
 
@@ -50,10 +72,10 @@ if __name__ == '__main__':
     # temp = cv2.imread(r'D:\Github\MyArknightsAutoHelper\image\开始唤醒.jpeg')
     # CV2无法识别中文路径
     temp = cv2.imdecode(
-        np.fromfile(r"D:\Github\MyArknightsAutoHelper\image\开始唤醒.jpeg",
+        np.fromfile('../image/开始唤醒.jpeg',
                     dtype=np.uint8), cv2.IMREAD_COLOR)
-    # target = cv2.imread(r'D:\Github\MyArknightsAutoHelper\lib\temp1.jpeg')
-    target = devices.get_screenshot()
-    a = tap_match_image(temp, target)
-    print(a)
-    devices.tap(a[0], a[1])
+    target = cv2.imread(r'temp3.jpeg')
+    # target = devices.get_screenshot()
+    print(exist_match_image(temp, target))
+    # print(a)
+    # devices.tap(a[0], a[1])
