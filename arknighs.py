a0a791ac3c8673f22a27df1346acd5944cbe70f3
DargonXUan <37977109+DargonXuan@users.noreply.github.com>
2021-08-09 21:03:20 +0800

    feat(arknights): 1.(tap_loaction)根据固定的屏幕坐标点击, 如果是二维数组(左上角,右上角)那么随机取点
    2. (signin)登录并完成签到,关闭公共及活动
    3. (back_to_index)在任意位置退回主页
    4. (receive_task)自动领取任务
    5. (riic)基建收菜
    6. (anni)从主页进入终端进入剿灭
    7. (resource)进入资源卡关

--- arknighs.py
+++ arknighs.py
@@ -9,13 +9,24 @@
 # @file: arknighs.py
 # @createtime: 2021/7/26 16:55 Green Sulley
 # @lastupdate: 2021/7/30 11:37 Green Sulley
-# @desc: Arknights Auto Helper based on ADB and Pytho
+# @desc: Arknights Auto Helper based on ADB and Python
 import yaml
+import time
+from typing import List
+
+from random import randint, uniform
+from loguru import logger
+
 from lib import adb
 from lib import utils
+from lib import recruitment  # 公招
+from lib import notifier
 
 with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
+
+with open('data/tap_location.yaml') as f:
+    data_tap_location = yaml.load(f, Loader=yaml.FullLoader)
 
 
 class Player:
@@ -24,24 +35,234 @@ class Player:
             "u": username,
             "p": password
         }
-        self.devices = adb.AndroidDebugBridge('9887bc394436343530')
+        self.devices = adb.AndroidDebugBridge(devices_id)
 
-    def login(self):
-        temp = utils.img_np_cv2('image/开始唤醒.jpeg')
-        target = self.devices.get_screenshot()
-        a = utils.tap_match_image(temp, target)
-        print(a)
+    def tap_loaction(self, *args: str) -> bool:
+        """
+        根据固定的屏幕坐标点击, 如果是二维数组(左上角,右上角)那么随机取点
+        :param args: 数据逐层解包
+        :return: 是否成功
+        """
+        value = data_tap_location
+        try:
+            for arg in args:
+                value = value[arg]
+            if isinstance(value[0], list):
+                c = uniform(value[0][0], value[1][0])
+                d = uniform(value[0][1], value[1][1])
+                self.devices.tap(c, d)
+            else:
+                self.devices.tap(value[0], value[1])
+        except:
+            logger.debug(f"点按{value[0]}{value[1]}失败")
+            return False
+        else:
+            logger.info(f"点按{value[0]}{value[1]}")
+            return True
+
+    def signin(self):
+        """
+        登录并完成签到,关闭公共及活动
+        :return:
+        """
+        while False:
+            start_exist = utils.match_image(
+                utils.img_np_cv2('image/start.jpeg'),
+                self.devices.get_screenshot())
+            if start_exist:
+                self.devices.tap(start_exist[0], start_exist[1])
+                return True
+            else:
+                time.sleep(30)  # 30秒检查一次
+        while False:
+            wake = utils.match_image(utils.img_np_cv2('image/开始唤醒.jpeg'),
+                                     self.devices.get_screenshot())
+            if wake:
+                self.devices.tap(wake[0], wake[1])
+                return True
+            else:
+                time.sleep(30)  # 30秒检查一次
+        inindex = False  # 是否处于主页
+        while inindex is False:
+            ter = utils.exist_match_image(utils.img_np_cv2('image/终端.jpeg'),
+                                          self.devices.get_screenshot())
+            if ter:
+                inindex = True
+            else:
+                pass  # todo
+        return True
+
+    def back_to_index(self):
+        """
+        在任意位置退回主页
+        :return:
+        """
+        a = utils.match_image(utils.img_np_cv2('image/主页标签.jpeg'),
+                              self.devices.get_screenshot())
         if a:
             self.devices.tap(a[0], a[1])
-        else:
-            pass
+            b = utils.match_image(utils.img_np_cv2('image/主页标签2.jpeg'),
+                                  self.devices.get_screenshot())
+            if b:
+                self.devices.tap(b[0], b[1])
+        inindex = False  # 是否处于主页
+        while inindex is False:
+            ter = utils.exist_match_image(utils.img_np_cv2('image/终端.jpeg'),
+                                          self.devices.get_screenshot())
+            if ter:
+                inindex = True
+            else:
+                pass  # todo
+        return True
 
-    @classmethod
-    def switch(cls):
-        pass
+    def public_recruitment(self):
+        """
+        公共招募,对接OCR
+        :return:
+        """
+        screenshot = self.devices.get_screenshot()[550:725, 530:1290]
+        tags = recruitment.ocr_tags(utils.cv2_to_base64(screenshot))
+        selected_tags = recruitment.recruitment(tags)
+        print(selected_tags)
+
+    def fixed_start(self):
+        """
+        在卡关界面,进入卡关
+        :return: 是否成功
+        """
+        screenshot = self.devices.get_screenshot()
+        proxy = utils.exist_match_image(utils.img_np_cv2('image/代理指挥开启.jpeg'),
+                                        screenshot)  # 在卡关界面判断是否可代理
+        if proxy:
+            location = utils.match_image(utils.img_np_cv2(
+                'image/开始行动.jpeg'), screenshot)  # 点击开始行动
+            del screenshot
+            if location:
+                self.devices.tap(location[0], location[1])
+                time.sleep(0.5)  # 等待进入
+                location2 = utils.match_image(
+                    utils.img_np_cv2('image/开始行动干员.jpeg'),
+                    self.devices.get_screenshot())
+                if location2:
+                    self.devices.tap(location2[0], location2[1])
+        else:
+            return False
+        over = False
+        while over is False:  # while not over
+            screenshot = self.devices.get_screenshot()
+            if utils.exist_match_image(utils.img_np_cv2('image/行动结束.jpeg'),
+                                       screenshot):
+                self.devices.tap(randint(100, 1800),
+                                 randint(100, 1080))  # 随机点出结算屏幕
+                time.sleep(2)  # 结算点出后,加载的时间
+                # todo 如果还未加载出卡关
+                print("已回到主卡关")
+                over = True
+            else:
+                time.sleep(10)  # 等待下一次屏幕检查
+        return True
+
+    def receive_task(self):
+        """
+        自动领取任务
+        :return:
+        """
+        self.tap_loaction('index', 'task')
+        target = self.devices.get_screenshot()
+        temp = utils.img_np_cv2('image/日常任务.jpeg')
+        a = utils.match_image(temp, target)
+        if a:
+            self.devices.tap(a[0], a[1])
+        temp = utils.img_np_cv2('image/周常任务.jpeg')
+        a = utils.match_image(temp, target)
+        if a:
+            self.devices.tap(a[0], a[1])
+        temp = utils.img_np_cv2('image/主线任务.jpeg')
+        a = utils.match_image(temp, target)
+        if a:
+            self.devices.tap(a[0], a[1])
+
+    def recruitment(self):
+        while False:
+            location = utils.match_image(utils.img_np_cv2(
+                'image/公开招募.jpeg'), self.devices.get_screenshot())  # 点击开始行动
+            if location:
+                self.devices.tap(location[0], location[1])
+                return True
+            else:
+                self.back_to_index()
+
+
+    def riic(self):
+        """
+        基建收菜
+        :return:
+        """
+        location = utils.match_image(utils.img_np_cv2(
+            'image/基建.jpeg'), self.devices.get_screenshot())  # 点击开始行动
+        self.devices.tap(location[0], location[1])
+        time.sleep(5)  # 保守点,等5秒
+        location = utils.match_image(utils.img_np_cv2(
+            'image/基建/基建待办.jpeg'), self.devices.get_screenshot())  # 点击开始行动
+        self.devices.tap(location[0], location[1])
+        for i in range(3):
+            self.tap_loaction('riic', 'matter')
+            time.sleep(1)
+        return True
+
+    def anni(self, rtype="a"):
+        """
+        从主页进入终端进入剿灭
+        :param: rtype a为切尔诺伯格 b为龙门市区
+        :return:
+        """
+        location = utils.match_image(utils.img_np_cv2(
+            'image/终端.jpeg'), self.devices.get_screenshot())  # 点击开始行动
+        if location:
+            self.devices.tap(location[0], location[1])
+            del location
+            time.sleep(0.1)  # 等待进入
+            self.tap_loaction('terminal', 'anni', 'button')
+            time.sleep(0.2)  # 等待进入
+            self.tap_loaction('terminal', 'anni', 'current')
+            time.sleep(0.3)  # 等待进入
+            self.tap_loaction('terminal', 'anni', 'blank')
+            time.sleep(0.3)  # 等待进入
+            self.tap_loaction('terminal', 'anni', 'map')
+            time.sleep(0.3)  # 等待进入
+            if rtype == "a":
+                qenbg = utils.match_image(
+                    utils.img_np_cv2('image/剿灭/切尔诺伯格.jpeg'),
+                    self.devices.get_screenshot())
+                if qenbg:
+                    self.devices.tap(qenbg[0], qenbg[1])
+        time.sleep(3)
+        return True
+
+    def resource(self, rtype: int):
+        """
+
+        :param rtype: 1为空中威胁 2为货物运送 3为粉碎防御 4为资源保障
+        :return:
+        """
+        location = utils.match_image(utils.img_np_cv2(
+            'image/终端.jpeg'), self.devices.get_screenshot())  # 点击开始行动
+        if location:
+            self.devices.tap(location[0], location[1])
+            del location
+            time.sleep(0.1)  # 等待进入
+            self.devices.tap(data_tap_location["terminal"]["resource"][0],
+                             data_tap_location["terminal"]["resource"][1])
+            time.sleep(0.2)
+            # 向左滑动,使四个资源全部展示在屏幕上
+            temp = utils.img_np_cv2(f'image/资源收集/{rtype}y.jpeg')
+            location = utils.match_image(temp, self.devices.get_screenshot())
+            if location:
+                self.devices.tap(location[0], location[1])
 
 
 if __name__ == '__main__':
     xuan = Player(config["account"][1]["username"],
                   config["account"][1]["password"], '9887bc394436343530')
-    xuan.login()
+    # xuan.login()
+    xuan.receive_task()
