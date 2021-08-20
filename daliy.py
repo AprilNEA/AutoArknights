92b5032a92bced200f31acbfa240936bac7b6b12
DargonXUan <37977109+DargonXuan@users.noreply.github.com>
2021-08-20 20:45:19 +0800

    Update daliy.py

--- daliy.py
+++ daliy.py
@@ -12,22 +12,16 @@
 # @desc: Arknights Auto Helper based on ADB and Python
 # @desc:
 import time
-from datetime import datetime, timezone, timedelta
+
+from loguru import logger
+
+from lib import utils
 from arknighs import Player
 
 
-def get_bjtime():
-    tn = time.localtime(time.time())
-    return {
-        "daytime": f"{tn.tm_year}-{tn.tm_mon}-{tn.tm_mday}",
-        "mday": int(tn.tm_mday),
-        "hour": int(tn.tm_hour),
-        "wday": int(tn.tm_wday)
-    }
-
-
 def daliy_limit():
-    tn = get_bjtime()
+    player = Player('9887bc394436343530')
+    tn = utils.get_bjtime()
     start1 = False
     while start1 is False:
         if 20 >= tn["hour"] >= 8:
@@ -40,14 +34,17 @@ def daliy_limit():
                 pass
             else:
                 pass
-            # 龙门
+            player.anni("a")
             if tn["wday"] == 1:
-                pass
+                for t in range(2):
+                    player.resource(3)  # 粉碎防御
             if tn["wday"] == 2:
-                pass
+                for t in range(2):
+                    player.resource(1)  # 粉碎防御
             if tn["wday"] == 5:
-                pass
-
+                for t in range(2):
+                    player.resource(5)  # 粉碎防御
+        # 4-4x1
         if tn["wday"] == 6:  # 周六
             pass
         if tn["wday"] == 0:  # 周日
@@ -61,7 +58,7 @@ def daliy_limit():
                 pass
             if tn["wday"] in [3, 4]:
                 pass
-            if tn["wday"] in [5,6]:
+            if tn["wday"] in [5, 6]:
                 pass
             if tn["wday"] == 0:
                 pass
@@ -78,16 +75,17 @@ def quick(times=1):
     player = Player(1, 1, '9887bc394436343530')
     for i in range(times):
         player.fixed_start()
-        print(f"共{times}次 第{i}次")
+        logger.info(f"共{times}次 第{i + 1}次")
 
 
 if __name__ == '__main__':
     # daliy_nolimit()
-    # quick(10)
+    quick(50)
     # print(get_bjtime())
-    player = Player(1, 1, '9887bc394436343530')
-    # a = player.anni()
-    # print(a)
-    # if a:
-    #     player.fixed_start()
-    player.riic()
+    # player = Player(1, 1, '9887bc394436343530')
+    # # a = player.anni()
+    # player.receive_task()
+    # # print(a)
+    # # if a:
+    # #     player.fixed_start()
+    # player.riic()
